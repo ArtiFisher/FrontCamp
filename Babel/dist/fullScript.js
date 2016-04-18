f1b16a7b2bd5676b282cea8de1cafd6a32013830
Artsiom Rybakou <Artsiom.Rybakou@thomsonreuters.com>
2016-04-18 17:37:24 +0300

    my-babel-plugin

--- Babel/dist/fullScript.js
+++ Babel/dist/fullScript.js
@@ -13,7 +13,7 @@ var Main = function Main() {
 	this.dropdown = document.getElementById('dropdown');
 	this.key = 'ed43227ea7d2b47a3757c4134e3e9c06:2:74941862';
 	new Theme(this.container, 'business', this.key);
-	this.dropdown.addEventListener('input', function (e) {
+	this.dropdown.addEventListener('change', function (e) {
 		return new Theme(_this.container, e.target.value, _this.key);
 	});
 };
