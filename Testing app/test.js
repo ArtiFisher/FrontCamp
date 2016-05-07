1269d229899839fc5948c85f735778dee56d5aa2
Fisher <artemis.fisher@gmail.com>
2016-05-07 22:36:37 +0300

    tested my app

---
+++ Testing app/test.js
@@ -0,0 +1,69 @@
+'use strict'
+
+describe('top stories', function(){
+  let app,
+    container,
+    dropdown;
+
+  beforeEach(() => {
+    container = document.createElement('div');
+    dropdown = document.createElement('select');
+    app = new Main(container, dropdown, 'ed43227ea7d2b47a3757c4234e3e9c06:2:74941862');
+  });
+
+  it('app should be inititalized properly', function(){
+    expect(app.model).toBeDefined();
+    expect(app.view).toBeDefined();
+    expect(app.mediator).toBeDefined();
+  });
+
+  it('should call request on message', function(){
+    spyOn(app.model, 'request');
+    app.mediator.send('new theme', 'technology');
+    expect(app.model.request).toHaveBeenCalledWith('technology');
+  });
+
+  it('should call display on message', function(){
+    spyOn(app.view, 'display');
+    app.mediator.send('items', 'content');
+    expect(app.view.display).toHaveBeenCalled();
+  });
+
+  it('should create items on display', function(){
+    let obj1 = {
+      url:'url1',
+      title:'title1',
+      abstract:'abstract1',
+      byline:'byline1'
+    };
+    spyOn(app.view, 'createItem');
+    app.view.display([obj1]);
+    expect(app.view.createItem).toHaveBeenCalledWith(obj1);
+  });
+
+  it('should create correct item', function(){
+    let obj1 = {
+      url:'url1',
+      title:'title1',
+      abstract:'abstract1',
+      byline:'byline1'
+    },
+    	expected = `<section class="item">
+		        <h2 class="title"><a href="url1">title1</a></h2>
+		        <article class="content">
+		          <p class="text">abstract1</p>
+		        </article>
+		        <div class="info">
+		          <span class="author">byline1</span>
+		        </div>
+		      </section>`,
+    	result = app.view.createItem(obj1);
+    expect(result).toMatch(expected);
+  });
+
+  it('should get correct item from factory', function(){
+    spyOn(app.model, 'request');
+    app.mediator.send('new theme', 'technology');
+    expect(app.model.request).toHaveBeenCalledWith('technology');
+  });
+});
---
+++ Testing app/topStories.js
@@ -0,0 +1,93 @@
+{
+	let instance = null;
+	class Main {
+		constructor(container, dropdown, key){
+			if(!instance){
+				instance = this;
+				this.mediator = new Mediator();
+				this.model = new Requester(key, this.mediator);
+				this.view = new Displayer(container, dropdown, this.mediator);
+				this.mediator.addListener(this.model, ['new theme']);
+				this.mediator.addListener(this.view, ['items']);
+				this.mediator.send('new theme', 'business');
+			}
+			return instance;
+		}
+	}
+
+	class Requester {
+		constructor(key, mediator){
+			this.mediator = mediator;
+			this.key = key;
+		}
+
+		receive(header, content){
+			if(header == 'new theme'){
+				this.request(content);
+			}
+		}
+
+		request(theme) {
+			return fetch('http://api.nytimes.com/svc/topstories/v1/' + theme + '.json?api-key=' + this.key, {
+				method: 'get'
+			})
+				.then(response => response.json())
+				.then(json => this.mediator.send('items', json.results));
+		}
+	}
+
+	class Displayer {
+		constructor(container, dropdown, mediator) {
+			this.container = container;
+			this.dropdown = dropdown;
+			this.mediator = mediator;
+			this.dropdown.addEventListener('input', e => this.mediator.send('new theme', e.target.value));
+		}
+
+		receive(header, content){
+			if(header == 'items'){
+				this.display(content);
+			}
+		}
+
+		display(items){
+			var HTML = '';
+			items.forEach(item => HTML += this.createItem(item));
+			this.container.innerHTML = HTML;
+			setTimeout(e => this.container.classList.add('show'), 0);
+		}
+
+		createItem(item){
+			var section = `<section class="item">
+		        <h2 class="title"><a href="${item.url}">${item.title}</a></h2>
+		        <article class="content">
+		          <p class="text">${item.abstract}</p>
+		        </article>
+		        <div class="info">
+		          <span class="author">${item.byline}</span>
+		        </div>
+		      </section>`;
+			return section;
+		}
+	}
+
+	class Mediator {
+		constructor(){
+			this.mailList = {};
+		}
+		addListener(listener, events){
+			for(var i = 0, len = events.length; i < len; i++){
+				!this.mailList[events[i]] && (this.mailList[events[i]] = []);
+				this.mailList[events[i]].push(listener);
+			}
+		}
+		send(header, content){
+			var listeners = this.mailList[header];
+			for(var i = 0, len = listeners.length; i < len; i++){
+				listeners[i].receive(header, content);
+			}
+		}
+	}
+
+	window.Main = Main;
+};
